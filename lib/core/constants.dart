4518c19ac359d615068e3a49469e46fb6d6355b4
Dylan10pc <77273699+Dylan10pc@users.noreply.github.com>
2025-01-22 22:03:06 +0000

    Adding things to constant still need to add more

---
+++ lib/core/constants.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+//main light background colors
+class MainLightcolor {
+  static const Color mainlightbackgroundcolor = Color.fromRGBO(254, 253, 238, 1.0);
+  static const Color mainlightprimarycolor = Color.fromRGBO(102, 211, 74, 1);
+  static const Color mainlightsecondarycolor = Color.fromRGBO(148, 232, 135, 1);
+  static const Color mainlightaccentcolor = Color.fromRGBO(105, 226, 139, 1.0);
+  static const Color maintextlightcolor = Color.fromRGBO(22, 25, 37, 1.0);
+}
+
+
+//main dark background colors
+class MainDarkcolor{
+  static const Color maindarkbackgroundcolor = Color.fromRGBO(7, 15, 43, 1.0);
+  static const Color maindarkprimarycolor = Color.fromRGBO(27, 26, 85, 1);
+  static const Color maindarksecondarycolor = Color.fromRGBO(83, 92, 145, 1);
+  static const Color maindarkaccentcolor = Color.fromRGBO(146, 144, 195, 1.0);
+  static const Color maindarktextdarkcolor = Color.fromRGBO(255, 255, 255, 1);
+
+
+}
+
+//notif for any messages 
+class Messagecolor {
+  static const Color errornotifcolor = Color.fromRGBO(242, 67, 51, 1.0);
+  static const Color successnotifcolor = Color.fromRGBO(64, 249, 155, 1.0);
+  static const Color warningnotifcolor = Color.fromRGBO(255, 179, 71, 1.0);
+}
+
+//text fonts
+class TextFonts {
+  static const TextStyle headingBalloBhaijann2 = TextStyle(
+    fontFamily: 'BalloBhaijann2',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle normalBalloBhaijann2 = TextStyle(
+    fontFamily: 'BalloBhaijann2',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle headingAfacadFlux = TextStyle(
+    fontFamily: 'AfacadFlux',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle normalAfacadFlux = TextStyle(
+    fontFamily: 'AfacadFlux',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle headingEncodeSans =TextStyle(
+    fontFamily: 'EncodeSans',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle normalEncodeSans = TextStyle(
+    fontFamily: 'EncodeSans',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+}
+
+//just some default values
+class Defaultdimensions {
+  static const double padding = 10;
+  static const double margin = 10;
+  static const double fontsize = 16;
+  static const double headingfontsize = 20;
+  static const double borderradius = 8;
+}
+
+class Approutes {
+
+}
+
+class API {
+
+}
+
+class DefaultValues {
+  
+}
